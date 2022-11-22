5ff69e52ca04ef4bec571518b542a5ca746b0d32
RookieAND <gwangin1999@naver.com>
2022-11-22 19:02:27 +0900

    feat : jwt 발급 및 유효성 검사 모듈 제작
    JWT를 발급하고, 이를 디코딩하여 유효성 검사를 진행하는 모듈을 제작했습니다.

--- BackEnd/src/auth/jwt.ts
+++ BackEnd/src/auth/jwt.ts
@@ -1,12 +1,36 @@
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 
+import { UnauthorizedError } from '@/errors/definedErrors';
+import User from '@/database/entity/user';
+
 dotenv.config();
 
 /**
  * 새로운 JWT를 생성하는 함수
+ *
+ * @param user JWT 생성을 위한 User 데이터
+ * @returns User 데이터로 생성된 JWT
  */
-export const createJWT = (user: string) => {
+export const createJWT = (user: User) => {
   const token = jwt.sign(user, process.env.JWT_SECRET_KEY!);
   return token;
+};
+
+/**
+ * 기존의 JWT를 디코딩하여 유저의 UUID를 반환하는 함수
+ *
+ * @param token 디코딩을 진행할 JWT
+ * @returns JWT를 디코딩하여 나온 유저의 UUID
+ */
+export const verifyJWT = async (token: string) => {
+  try {
+    const decoded_token = jwt.verify(
+      token,
+      process.env.JWT_SECRET_KEY!,
+    ) as User;
+    return decoded_token.uuid;
+  } catch (err) {
+    throw new UnauthorizedError('유효하지 않은 JWT 입니다.')
+  }
 };
