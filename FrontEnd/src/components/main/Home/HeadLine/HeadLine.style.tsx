03ebd9be52f346ad7eceb4fec47efcd6efb2b300
RookieAND <gwangin1999@naver.com>
2022-11-28 22:48:37 +0900

    feat : 메인 페이지 헤더라인 레이아웃 제작 (#8)
    메인 페이지에 삽입될 헤더라인 레이아웃을 제작했습니다.

---
+++ FrontEnd/src/components/main/Home/HeadLine/HeadLine.style.tsx
@@ -0,0 +1,71 @@
+import { StaticImageData } from "next/image";
+import styled, { css } from "styled-components";
+
+interface StyledHeadlineWrapperProps {
+  image: StaticImageData;
+}
+
+export const Wrapper = styled.div<StyledHeadlineWrapperProps>`
+  ${({ theme, image }) => {
+    const { colors } = theme;
+    return css`
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: flex-start;
+      padding: 160px 0px 160px 112px;
+      gap: 48px;
+
+      width: 100%;
+      height: 700px;
+
+      background: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.25)),
+        url(${image.src});
+      background-size: cover;
+    `;
+  }}
+`;
+
+export const TextBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 4px;
+
+  width: 90%;
+  height: 149px;
+`;
+
+export const MainText = styled.h5`
+  ${({ theme }) => {
+    const { colors, fonts } = theme;
+    return css`
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      margin: 0px;
+      padding: 0px;
+      gap: 4px;
+
+      color: ${colors.mono.white};
+      font-weight: ${fonts.weight.bold};
+      font-size: ${fonts.size.xxl};
+    `;
+  }}
+`;
+
+export const SubText = styled.p`
+  ${({ theme }) => {
+    const { colors, fonts } = theme;
+    return css`
+      width: 1328px;
+      margin: 0px;
+      padding: 0px;
+
+      font-weight: ${fonts.weight.normal};
+      font-size: ${fonts.size.lg};
+
+      color: ${colors.mono.white};
+    `;
+  }}
+`;
