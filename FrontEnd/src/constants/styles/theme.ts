91ce16a318b9045bc2183ad90f8da0e95f0a1c8d
RookieAND <gwangin1999@naver.com>
2022-11-28 17:28:43 +0900

    fix : styled-component 전역 theme 변수 타입 수정
    styled-component 에서 전역적으로 관리되는 theme 변수 타입을 일부 수정했습니다.

--- FrontEnd/src/constants/styles/theme.ts
+++ FrontEnd/src/constants/styles/theme.ts
@@ -1,40 +1,56 @@
 import { DefaultTheme } from "styled-components";
 
-export const theme: DefaultTheme = {
-  fonts: {
-    size: {
-      sm: "14px",
-      base: "16px",
-      lg: "18px",
-      xl: "22px",
-      xxl: "28px",
-      title: "36px"
-    },
-    weight: {
-      light: 100,
-      normal: 400,
-      bold: 700
-    }
-  },
+export type ColorType = typeof colors;
+export type FontType = typeof fonts;
+export type DeviceType = typeof devices;
 
-  colors: {
-    mono: {
-      white: "#FFFFFF",
-      gray1: "#F5F5F5",
-      gray2: "#EDEDED",
-      gray3: "#E0E0E0",
-      gray4: "#C2C2C2",
-      gray5: "#9E9E9E",
-      gray6: "#757575",
-      gray7: "#616161",
-      gray8: "#424242",
-      black: "#1D1D1D"
-    }
+const colors = {
+  mono: {
+    white: "#FFFFFF",
+    gray1: "#F5F5F5",
+    gray2: "#EDEDED",
+    gray3: "#E0E0E0",
+    gray4: "#C2C2C2",
+    gray5: "#9E9E9E",
+    gray6: "#757575",
+    gray7: "#616161",
+    gray8: "#424242",
+    black: "#1D1D1D"
   },
-
-  device: {
-    mobile: `@media only screen and (max-width: 425px)`,
-    tablet: `@media only screen and (max-width: 768px)`,
-    desktop: `@media only screen and (max-width: 1440px)`
+  main: {
+    normal: "#036B29",
+    pressed: "#044E1F"
   }
+};
+
+const fontWeight = {
+  light: 100,
+  normal: 400,
+  bold: 700
+};
+
+const fontSize = {
+  sm: "14px",
+  base: "16px",
+  lg: "18px",
+  xl: "22px",
+  xxl: "28px",
+  title: "36px"
+};
+
+const fonts = {
+  weight: fontWeight,
+  size: fontSize
+};
+
+const devices = {
+  mobile: `@media only screen and (max-width: 425px)`,
+  tablet: `@media only screen and (max-width: 768px)`,
+  desktop: `@media only screen and (max-width: 1440px)`
+};
+
+export const theme: DefaultTheme = {
+  colors,
+  fonts,
+  devices
 };
