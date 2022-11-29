b619034d6dbb5103d5d8b330fe01ce1d762de295
RookieAND <gwangin1999@naver.com>
2022-11-29 18:07:28 +0900

    feat : 공용으로 사용될 푸터 레이아웃 제작 (#8)
    모든 페이지에 공통적으로 사용될 푸터 레이아웃을 1차적으로 제작했습니다.

---
+++ FrontEnd/src/components/common/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import * as style from "./Footer.style";
+
+const Footer = () => {
+  return (
+    <style.Wrapper>
+      <style.AuthInfo>
+        <strong>@ KU : AGORA</strong> By RookieAND
+      </style.AuthInfo>
+      <style.AuthDetail>건국인이 함께 만들어가는 지식의 요람.</style.AuthDetail>
+    </style.Wrapper>
+  );
+};
+
+export default Footer;
