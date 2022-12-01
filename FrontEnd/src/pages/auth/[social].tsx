5df1e78dcc6906f52fba640373cc74795eefcc3b
RookieAND <gwangin1999@naver.com>
2022-12-01 22:35:59 +0900

    feat : OAuth2 에 필요한 변수를 상수화
    OAuth2에 필요한 값들을 모아 상수 파일로 이전했습니다.

--- FrontEnd/src/pages/auth/[social].tsx
+++ FrontEnd/src/pages/auth/[social].tsx
@@ -1,18 +1,20 @@
 import { useRouter } from "next/router";
 import { NextPage } from "next";
-import { useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 
-import { accessTokenSelector } from "@/stores/auth";
+import { accessTokenAtom } from "@/stores/auth";
+import { postAsync } from "@/apis/API";
 import { SocialPlatform } from "@/apis/OAuth2/auth";
 import LoginTemplate from "@/components/template/LoginTemplate/LoginTemplate";
 
 const SocialLogin: NextPage = () => {
   const router = useRouter();
-  const socialType = router.query.social as SocialPlatform;
   const code = router.query.code;
+  const social = router.query.social;
+
 
   // 이미 로그인이 되어 있다면, 이전 화면으로 되돌림.
-  const accessToken = useRecoilValue(accessTokenSelector);
+  const [accessToken, setAccessToken] = useRecoilState(accessTokenAtom);
   if (accessToken != null) {
     () => router.back();
   }
