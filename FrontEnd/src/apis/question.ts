c04dfc16ec01af84db70408013dfea0006dfddea
RookieAND <gwangin1999@naver.com>
2022-12-06 01:17:40 +0900

    feat : 신규 질문글 추가 모듈 제작
    새로운 질문글을 DB에 추가시키는 모듈을 제작하였습니다.

--- FrontEnd/src/apis/question.ts
+++ FrontEnd/src/apis/question.ts
@@ -1,9 +1,7 @@
-import { getAsync } from "./API";
+import { getAsync, postAsync } from "./API";
 
 export interface QuestionPostType {
   title: string;
-  content: string;
-  nickname: string;
   createdAt: string;
   likes: number;
   comments: number;
@@ -11,15 +9,24 @@ export interface QuestionPostType {
   state: "progressed" | "completed";
 }
 
-interface AddQuestionType {
+export interface QuestionDetailType extends QuestionPostType {
+  content: string;
+  nickname: string;
+  isLike: boolean;
+}
+
+export interface AddQuestionType {
   title: string;
   content: string;
-  uuid: string;
+}
+
+export interface AddQuestionResultType {
+  questionId: number;
 }
 
 export type QuestionSortType = "recent" | "popular";
 
-export const getQuestionsAsync = async (
+export const getQuestionListAsync = async (
   page: number,
   amount: number,
   option: QuestionSortType
@@ -28,16 +35,46 @@ export const getQuestionsAsync = async (
     params: { page, amount, option }
   });
 
-  if (response.isSuccess) {
-    return {
-      isSuccess: response.isSuccess,
-      result: {
-        questions: response.result,
-        page,
-        amount
-      }
-    };
-  }
+  return {
+    isSuccess: response.isSuccess,
+    result: {
+      questions: response.isSuccess ? response.result : [],
+      page,
+      amount
+    }
+  };
+};
+
+export const getQuestionAsync = async (questionId: number) => {
+  const response = await getAsync<QuestionDetailType, unknown>(
+    `/question/${questionId}`
+  );
 
   return response;
+};
+
+/**
+ * 작성된 질문글을 새롭게 추가하는 함수
+ * @param title 질문글 제목
+ * @param content 질문글 내용
+ * @param token 질문글을 작성한 유저의 엑세스 토큰
+ * @returns
+ */
+export const addQuestionAsync = async (
+  title: string,
+  content: string,
+  token: string
+) => {
+  const response = await postAsync<AddQuestionResultType, AddQuestionType>(
+    `question/write`,
+    {
+      title,
+      content
+    },
+    { headers: { authorization: token } }
+  );
+  if (response.isSuccess) {
+    return response.result.questionId;
+  }
+  return null;
 };
