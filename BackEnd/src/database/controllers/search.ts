caa69179380464b81bf6cc45f0296d164d256dac
RookieAND <gwangin1999@naver.com>
2022-12-05 17:18:05 +0900

    feat : 질문글 검색 관련 DB 컨트롤러 추가
    특정 단어에 대한 질문글을 검색하여 결과를 반환시키는 DB 컨트롤러를 추가했습니다.

---
+++ BackEnd/src/database/controllers/search.ts
@@ -0,0 +1,45 @@
+import { getRepository } from 'typeorm';
+import Question from '../entity/question';
+
+export const searchQuestionByTitle = async (
+  title: string,
+  page: number,
+  amount: number,
+  option: 'recent' | 'popular',
+) => {
+  const sortType = {
+    recent: 'question.createdAt',
+    popular: 'likeAmount',
+  };
+
+  const questionsByTitle = await getRepository(Question)
+    .createQueryBuilder('question')
+    .select([
+      'question.id',
+      'question.title',
+      'question.content',
+      'question.state',
+      'question.createdAt',
+      'question.updatedAt',
+    ])
+    .innerJoin(
+      (qb) =>
+        qb
+          .select(['subquestion.id', 'COUNT(likes.id) AS likeCount'])
+          .from(Question, 'subquestion')
+          .where('subquestion.title like :title', { title: `%${title}` })
+          .leftJoin('subquestion.likes', 'likes')
+          .groupBy('subquestion.id')
+          .offset((page - 1) * amount)
+          .limit(amount)
+          .orderBy(sortType[option], 'DESC'),
+      'topQuestion',
+      'topQuestion.id == question.id',
+    )
+    .leftJoinAndSelect('question.user', 'user')
+    .leftJoinAndSelect('question.category', 'category')
+    .loadRelationCountAndMap('question.likes', 'likeCount')
+    .getMany();
+
+  return questionsByTitle;
+};
