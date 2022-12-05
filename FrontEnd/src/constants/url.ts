d79a504c34f9800c8bf472fa62100aaf26c02d5e
RookieAND <gwangin1999@naver.com>
2022-12-05 18:37:10 +0900

    feat : 로그아웃 관련 네비게이션 아이콘 추가
    로그아웃 관련 네비게이션 아이콘을 추가하고 이를 적용했습니다.

--- FrontEnd/src/constants/url.ts
+++ FrontEnd/src/constants/url.ts
@@ -1,8 +1,10 @@
 export const PATH_INFO = {
   main: "/",
-  question: "/question",
+  question: "/questions",
+  write: "/write",
   profile: "/profile",
-  login: "/login"
+  login: "/login",
+  logout: "/logout"
 } as const;
 
 export type PathNameType = typeof PATH_INFO[keyof typeof PATH_INFO];
