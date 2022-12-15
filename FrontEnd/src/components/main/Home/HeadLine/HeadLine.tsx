959ac8d69049bb90de71ee46921bc2d9134421ff
RookieAND <gwangin1999@naver.com>
2022-12-16 03:38:33 +0900

    feat : 메인 페이지 버튼에 페이지 이동 기능 추가
    메인 페이지 버튼에 로그인 진행 시 글 작성 페이지, 비로그인 시 로그인 페이지로 이동하는 기능을 추가했습니다.

--- FrontEnd/src/components/main/Home/HeadLine/HeadLine.tsx
+++ FrontEnd/src/components/main/Home/HeadLine/HeadLine.tsx
@@ -12,22 +12,16 @@ const Headline = ({ isLogin }: HeadlineProps) => {
   const router = useRouter();
 
   const clickSubmitBtn = () => {
-    if (!isLogin) {
-      router.push("/login");
-    }
+    router.push(isLogin ? "/write" : "/login");
   };
 
   return (
     <style.Wrapper image={HeadLineImg}>
       <style.TextBox>
         <style.MainText>지식의 요람, KU : AGORA</style.MainText>
-        <style.SubText>
-          겹겹이 쌓인 질문 속에서 해답을 찾아보세요.
-        </style.SubText>
+        <style.SubText>겹겹이 쌓인 질문 속에서 해답을 찾아보세요.</style.SubText>
       </style.TextBox>
-      <style.SubmitButton onClick={clickSubmitBtn}>
-        지금 질문하기
-      </style.SubmitButton>
+      <style.SubmitButton onClick={clickSubmitBtn}>지금 질문하기</style.SubmitButton>
     </style.Wrapper>
   );
 };
