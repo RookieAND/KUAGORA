54165836430e845b75f74eb051fe8f8cc892531b
RookieAND <gwangin1999@naver.com>
2022-12-16 03:37:04 +0900

    feat : 사이트 안내 레이아웃을 메인 페이지에서 제거
    사이트 안내 레이아웃을 메인 페이지에서 임시 제거하였습니다.

--- FrontEnd/src/components/template/MainTemplate/MainTemplate.tsx
+++ FrontEnd/src/components/template/MainTemplate/MainTemplate.tsx
@@ -1,12 +1,11 @@
 import { useAtom } from "jotai";
 import { accessTokenAtom } from "@/stores/actions";
-import { getQuestionsAsync, QuestionPostType } from "@/apis/question";
+import { QuestionPostType } from "@/apis/question";
 
 import TitleBox from "@/components/common/TitleBox";
 import Navbar from "@/components/common/Navbar";
 import Footer from "@/components/common/Footer";
 import HeadLine from "@/components/main/Home/HeadLine";
-import Information from "@/components/main/Home/Information";
 import QuestionList from "@/components/main/Question/QuestionList";
 
 interface MainTemplateProps {
@@ -25,8 +24,6 @@ const MainTemplate = ({ recentQuestions, popularQuestions }: MainTemplateProps) 
       <QuestionList questions={recentQuestions} />
       <TitleBox title={"Popular Questions"} subTitle={"학우 분들에게 인기 있는 질문글 목록입니다."} />
       <QuestionList questions={popularQuestions} />
-      <TitleBox title={"Our Service"} subTitle={"KU : AGORA 의 서비스에 대한 설명입니다."} />
-      <Information />
       <Footer />
     </>
   );
