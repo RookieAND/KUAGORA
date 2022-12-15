2ef61782ba8622f595e0eb045f2352b4b6da4634
RookieAND <gwangin1999@naver.com>
2022-12-16 03:34:32 +0900

    feat : 인기글 / 최신글 목록을 보여주는 레이아웃 추가
    인기글 및 최신글 목록을 보여주는 레이아웃을 추가하였습니다.

--- FrontEnd/src/components/template/MainTemplate/MainTemplate.tsx
+++ FrontEnd/src/components/template/MainTemplate/MainTemplate.tsx
@@ -1,6 +1,6 @@
 import { useAtom } from "jotai";
 import { accessTokenAtom } from "@/stores/actions";
-import { QuestionPostType } from "@/apis/question";
+import { getQuestionsAsync, QuestionPostType } from "@/apis/question";
 
 import TitleBox from "@/components/common/TitleBox";
 import Navbar from "@/components/common/Navbar";
@@ -10,20 +10,23 @@ import Information from "@/components/main/Home/Information";
 import QuestionList from "@/components/main/Question/QuestionList";
 
 interface MainTemplateProps {
-  questions: QuestionPostType[];
+  recentQuestions: QuestionPostType[];
+  popularQuestions: QuestionPostType[];
 }
 
-const MainTemplate = ({ questions }: MainTemplateProps) => {
+const MainTemplate = ({ recentQuestions, popularQuestions }: MainTemplateProps) => {
   const [accessToken] = useAtom(accessTokenAtom);
 
   return (
     <>
       <Navbar />
       <HeadLine isLogin={accessToken != null} />
-      <TitleBox title={"Our Service"} subTitle={"지식의 요람. [KU : AGORA] 에 대해 소개합니다."} />
+      <TitleBox title={"Today Questions"} subTitle={"학우 분들이 최근에 등록한 질문글 목록입니다."} />
+      <QuestionList questions={recentQuestions} />
+      <TitleBox title={"Popular Questions"} subTitle={"학우 분들에게 인기 있는 질문글 목록입니다."} />
+      <QuestionList questions={popularQuestions} />
+      <TitleBox title={"Our Service"} subTitle={"KU : AGORA 의 서비스에 대한 설명입니다."} />
       <Information />
-      <TitleBox title={"Today Questions"} subTitle={"학우 분들이 가장 최근에 등록한 질문글 목록입니다."} />
-      <QuestionList questions={questions} />
       <Footer />
     </>
   );
