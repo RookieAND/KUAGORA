7d472c796a9eac9cf2dd462923d5868b539d2f87
RookieAND <gwangin1999@naver.com>
2022-12-16 03:33:23 +0900

    feat : 최신글 / 인기글 목록을 불러오는 로직 추가
    최신글, 인기글 목록을 페이지 로딩 시에 불러오도록 로직을 추가했습니다.

--- FrontEnd/src/pages/index.tsx
+++ FrontEnd/src/pages/index.tsx
@@ -4,18 +4,25 @@ import MainTemplate from "@/components/template/MainTemplate";
 import { QuestionPostType, getQuestionsAsync } from "@/apis/question";
 
 interface QuestionsPageProps {
-  questions: QuestionPostType[];
+  recentQuestions: QuestionPostType[];
+  popularQuestions: QuestionPostType[];
 }
 
-export async function getStaticProps() {
-  const response = await getQuestionsAsync(1, 8, "recent");
+export async function getServerSideProps() {
+  const [recentQuestions, popularQuestions] = await Promise.all([
+    getQuestionsAsync(1, 8, "recent"),
+    getQuestionsAsync(1, 8, "popular")
+  ]);
+
   return {
-    props: { questions: response.isSuccess ? response.result.content : [] }
+    props: {
+      recentQuestions: recentQuestions.isSuccess ? recentQuestions.result.content : [],
+      popularQuestions: popularQuestions.isSuccess ? popularQuestions.result.content : []
+    }
   };
 }
 
-const Home = ({ questions }: QuestionsPageProps) => {
-  console.log(questions);
+const Home = ({ recentQuestions, popularQuestions }: QuestionsPageProps) => {
   return (
     <>
       <Head>
@@ -25,7 +32,7 @@ const Home = ({ questions }: QuestionsPageProps) => {
         <link rel="icon" href="/favicon.ico" />
         <title>지식의 요람, KU : AGORA</title>
       </Head>
-      <MainTemplate questions={questions} />
+      <MainTemplate recentQuestions={recentQuestions} popularQuestions={popularQuestions} />
     </>
   );
 };
