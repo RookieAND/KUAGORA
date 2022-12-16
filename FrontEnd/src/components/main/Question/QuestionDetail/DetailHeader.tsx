1ef7c2bb8ef74384badaf7c71a35af70c5d61305
RookieAND <gwangin1999@naver.com>
2022-12-17 02:34:04 +0900

    feat : 좋아요 버튼 레이아웃 추가
    좋아요 버튼의 레이아웃을 추가했습니다.

--- FrontEnd/src/components/main/Question/QuestionDetail/DetailHeader.tsx
+++ FrontEnd/src/components/main/Question/QuestionDetail/DetailHeader.tsx
@@ -1,17 +1,21 @@
-import * as style from "./DetailHeader.style";
-
-import HeartSvg from "@/assets/icons/Heart.svg";
-
+import { LikeDataType } from "@/apis/question";
 import { formatISODate } from "@/utils/formatISODate";
+
+import * as style from "./DetailHeader.style";
+import LikeSvg from "@/assets/icons/Like.svg";
 
 interface DetailHeaderProps {
   title: string;
   state: "progressed" | "completed";
   nickname: string;
   createdAt: string;
+  likesData: LikeDataType;
+  toggleLikeState: () => void;
 }
 
-const DetailHeader = ({ title, state, nickname, createdAt }: DetailHeaderProps) => {
+const DetailHeader = ({ title, state, nickname, createdAt, likesData, toggleLikeState }: DetailHeaderProps) => {
+  const { isLike, likeCount } = likesData;
+
   return (
     <style.Wrapper>
       <style.TitleBox>
@@ -23,6 +27,10 @@ const DetailHeader = ({ title, state, nickname, createdAt }: DetailHeaderProps) 
           <style.StateText>작성자 | {nickname}</style.StateText>
           <style.StateText>{createdAt && `작성일 | ${formatISODate(createdAt)}`}</style.StateText>
         </style.StateBox>
+        <style.LikeBox isLike={isLike} onClick={toggleLikeState}>
+          <LikeSvg />
+          <style.LikeText>{likeCount}</style.LikeText>
+        </style.LikeBox>
         <style.IsCompleteBox state={state}>{state == "progressed" ? "미해결" : "해결됨"}</style.IsCompleteBox>
       </style.AuthorBox>
     </style.Wrapper>
