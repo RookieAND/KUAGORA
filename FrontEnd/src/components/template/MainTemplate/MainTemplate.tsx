a9ba1fd83baaf76db6191b0f5738308d84d8ad2e
RookieAND <gwangin1999@naver.com>
2022-12-16 20:42:02 +0900

    feat : 메인 페이지의 질문글 목록 기준을 수정했습니다 (#32)
    메인 페이지의 질문글 목록 기준을 최신 미답변, 최신 답변, 인기글 순으로 나열하였습니다.

--- FrontEnd/src/components/template/MainTemplate/MainTemplate.tsx
+++ FrontEnd/src/components/template/MainTemplate/MainTemplate.tsx
@@ -10,18 +10,21 @@ import QuestionList from "@/components/main/Question/QuestionList";
 
 interface MainTemplateProps {
   recentQuestions: QuestionPostType[];
+  recentAnsweredQuestion: QuestionPostType[];
   popularQuestions: QuestionPostType[];
 }
 
-const MainTemplate = ({ recentQuestions, popularQuestions }: MainTemplateProps) => {
+const MainTemplate = ({ recentQuestions, recentAnsweredQuestion, popularQuestions }: MainTemplateProps) => {
   const [accessToken] = useAtom(accessTokenAtom);
 
   return (
     <>
       <Navbar />
       <HeadLine isLogin={accessToken != null} />
-      <TitleBox title={"Today Questions"} subTitle={"학우 분들이 최근에 등록한 질문글 목록입니다."} />
+      <TitleBox title={"Recently Asked"} subTitle={"학우 분들이 최근에 등록한 질문글 목록입니다."} />
       <QuestionList questions={recentQuestions} />
+      <TitleBox title={"Recently Answered"} subTitle={"학우 분들이 최근에 답변한 질문글 목록입니다."} />
+      <QuestionList questions={recentAnsweredQuestion} />
       <TitleBox title={"Popular Questions"} subTitle={"학우 분들에게 인기 있는 질문글 목록입니다."} />
       <QuestionList questions={popularQuestions} />
       <Footer />
--- FrontEnd/src/pages/index.tsx
+++ FrontEnd/src/pages/index.tsx
@@ -5,24 +5,27 @@ import { QuestionPostType, getQuestionsAsync } from "@/apis/question";
 
 interface QuestionsPageProps {
   recentQuestions: QuestionPostType[];
+  recentAnsweredQuestion: QuestionPostType[];
   popularQuestions: QuestionPostType[];
 }
 
 export async function getServerSideProps() {
-  const [recentQuestions, popularQuestions] = await Promise.all([
+  const [recentQuestions, recentAnsweredQuestion, popularQuestions] = await Promise.all([
     getQuestionsAsync(1, 8, "recent", "progressed"),
-    getQuestionsAsync(1, 8, "popular", "completed")
+    getQuestionsAsync(1, 8, "recent", "completed"),
+    getQuestionsAsync(1, 8, "popular", "both")
   ]);
 
   return {
     props: {
       recentQuestions: recentQuestions.isSuccess ? recentQuestions.result.content : [],
+      recentAnsweredQuestion: recentAnsweredQuestion.isSuccess ? recentAnsweredQuestion.result.content : [],
       popularQuestions: popularQuestions.isSuccess ? popularQuestions.result.content : []
     }
   };
 }
 
-const Home = ({ recentQuestions, popularQuestions }: QuestionsPageProps) => {
+const Home = ({ recentQuestions, recentAnsweredQuestion, popularQuestions }: QuestionsPageProps) => {
   return (
     <>
       <Head>
@@ -32,7 +35,11 @@ const Home = ({ recentQuestions, popularQuestions }: QuestionsPageProps) => {
         <link rel="icon" href="/favicon.ico" />
         <title>지식의 요람, KU : AGORA</title>
       </Head>
-      <MainTemplate recentQuestions={recentQuestions} popularQuestions={popularQuestions} />
+      <MainTemplate
+        recentQuestions={recentQuestions}
+        recentAnsweredQuestion={recentAnsweredQuestion}
+        popularQuestions={popularQuestions}
+      />
     </>
   );
 };
