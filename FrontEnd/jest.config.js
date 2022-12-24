be85c2a457bf70a29399c0f00535935a369c0449
RookieAND <gwangin1999@naver.com>
2022-12-24 15:40:19 +0900

    feat : jest 가동에 필요한 추가 라이브러리 설치
    jest 가동에 필요한 추가 라이브러리를 설치하였습니다.

--- FrontEnd/jest.config.js
+++ FrontEnd/jest.config.js
@@ -1,40 +1,35 @@
-
 module.exports = {
-    collectCoverageFrom: [
-      "**/*.{js,jsx,ts,tsx}",
-      "!**/*.d.ts",
-      "!**/node_modules/**"
-    ],
-    moduleNameMapper: {
-      // Handle CSS imports (with CSS modules)
-      // https://jestjs.io/docs/webpack#mocking-css-modules
-      "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
-  
-      // Handle CSS imports (without CSS modules)
-      "^.+\\.(css|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
-  
-      // Handle image imports
-      // https://jestjs.io/docs/webpack#handling-static-assets
-      "^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$": `<rootDir>/__mocks__/fileMock.js`,
-  
-      // Handle module aliases
-      "^~/(.*)$": "<rootDir>/src/$1",
-      "^@pages/(.*)$": "<rootDir>/src/pages/$1",
-      "^@components/(.*)$": "<rootDir>/src/components/$1",
-      "^@assets/(.*)$" : "<rootDir>/src/assets/$1",
-      "^@utils/(.*)$" : "<rootDir>/src/utils/$1",
-      "^@hooks/(.*)$" : "<rootDir>/src/hooks/$1",
-      "^@constants/(.*)$" : "<rootDir>/src/constants/$1",
-      "^@apis/(.*)$" : "<rootDir>/src/apis/$1",
-    },
-    setupFiles:["<rootDir>/jest/globals.ts"],
-    // setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-    testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
-    testEnvironment: "jsdom",
-    transform: {
-      // Use babel-jest to transpile tests with the next/babel preset
-      // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
-      "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }]
-    },
-    transformIgnorePatterns: ["/node_modules/", "^.+\\.module\\.(css|sass|scss)$"]
-  };
+  collectCoverageFrom: ["**/*.{js,jsx,ts,tsx}", "!**/*.d.ts", "!**/node_modules/**"],
+  moduleNameMapper: {
+    // Handle CSS imports (with CSS modules)
+    // https://jestjs.io/docs/webpack#mocking-css-modules
+    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
+
+    // Handle CSS imports (without CSS modules)
+    "^.+\\.(css|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
+
+    // Handle image imports
+    // https://jestjs.io/docs/webpack#handling-static-assets
+    "^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$": `<rootDir>/__mocks__/fileMock.js`,
+
+    // Handle module aliases
+    "^~/(.*)$": "<rootDir>/src/$1",
+    "^@/pages/(.*)$": "<rootDir>/src/pages/$1",
+    "^@/components/(.*)$": "<rootDir>/src/components/$1",
+    "^@/assets/(.*)$": "<rootDir>/src/assets/$1",
+    "^@/utils/(.*)$": "<rootDir>/src/utils/$1",
+    "^@/hooks/(.*)$": "<rootDir>/src/hooks/$1",
+    "^@/constants/(.*)$": "<rootDir>/src/constants/$1",
+    "^@/apis/(.*)$": "<rootDir>/src/apis/$1"
+  },
+  // setupFiles:["<rootDir>/jest/globals.ts"],
+  // setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
+  testEnvironment: "jsdom",
+  transform: {
+    // Use babel-jest to transpile tests with the next/babel preset
+    // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
+    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }]
+  },
+  transformIgnorePatterns: ["/node_modules/", "^.+\\.module\\.(css|sass|scss)$"]
+};
