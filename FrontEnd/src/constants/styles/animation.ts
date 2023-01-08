de9c75aa48a89f5f6fd75d92e6f852b5d6c72c41
RookieAND <gwangin1999@naver.com>
2023-01-08 20:12:43 +0900

    feat: 툴바 생성 / 종료 시 실행될 애니메이션 추가
    툴바 생성 및 종료 시 실행될 애니메이션을 추가하였습니다.

--- FrontEnd/src/constants/styles/animation.ts
+++ FrontEnd/src/constants/styles/animation.ts
@@ -19,3 +19,31 @@ export const modalBgShow = keyframes`
     opacity: 1;
   } 
 `;
+
+export const toolbarShow = keyframes`
+  from {
+    opacity: 0.25;
+    transform: translate3d(20%, 0, 0);
+  }
+  80% {
+    transform: translate3d(-10%, 0, 0);
+  }
+  90% {
+    transform: translate3d(5%, 0, 0);
+  }
+  to {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+  }
+`;
+
+export const toolbarFinish = keyframes`
+  from {
+    opacity: 0.25;
+    transform: translate3d(0%, 0, 0);
+  }
+  to {
+    opacity: 1;
+    transform: translate3d(20%, 0, 0);
+  }
+`;
